c45a2ee4ae8aa8621e424c14c5b8abca6b3b9330
Joao Pedro <jpresende502@gmail.com>
2021-05-26 22:55:26 -0300

    feat: Ajusting the login controller.

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,12 +1,12 @@
 <?php
 require_once '../models/database_connection.php';
 
-$user_data = getUsernameAndPassword($_POST['username'], $_POST['password']);
-
-if (!empty($user_data)){
+if (checkLogin($_POST['email'], $_POST['password'])){
     session_start();
 
-    $_SESSION['username'] = $_POST['username'];
+    $_SESSION['email'] = $_POST['email'];
     echo "<script>location.href='../views/home.php'</script>";
+} else {
+    echo "<script>location.href='../views/index.php?error=true'</script>";
 }
 
