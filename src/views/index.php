73d5ea0883c6afe433d04f2bc0ec302f8be68487
Joao Pedro <jpresende502@gmail.com>
2021-05-26 22:50:26 -0300

    feat: Renaming the login/logon page and adding more logic in the page.

--- src/views/index.php
+++ src/views/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../../public/styles/index.css">
     <link rel="shortcut icon" href="../../public/favicon.png" />
     <title>Login & Cadastro | Ctrl+Play</title>
 </head>
@@ -12,7 +12,7 @@
     <header>
         <h2>Ctrl+Play</h2>
             <form action="/src/controllers/login.php" method="POST">
-                <input name="username" placeholder="Digite seu usuário">
+                <input name="email" placeholder="Digite seu Email">
                 <input name="password" placeholder="Digite sua senha" type="password">
 
                 <button type="submit">Login</button>
@@ -23,7 +23,7 @@
             <h1>Cadastre-se na Ctrl+Play!</h1>
             <img src="../../public/homer.gif" alt="homer dançando 2021 lol"/>
         </section>
-        <form action="/controller/register.php" method="POST">
+        <form action="../controllers/register.php" method="POST">
             <section class="first-section">
                 <div>
                     <label for="email">Email</label>
@@ -34,11 +34,17 @@
                     <input name="name" id="name">
                 </div>
             </section>
-            <div class="total-line">
-                <label for="username">Username</label>
-                <input name="username" id="username">
-            </div>
             <section class="secound-section">
+                <div>
+                    <label for="avatar_url">Avatar_url</label>
+                    <input name="avatar_url" id="avatar_url">
+                </div>
+                <div>
+                    <label for="username">Username</label>
+                    <input name="username" id="username">
+                </div>
+            </section>
+            <section class="third-section">
                 <div>
                     <label for="biograph">Biograph</label>
                     <input name="biograph" id="biograph">
@@ -68,3 +74,34 @@
     </section>
 </body>
 </html>
+<?php
+    if(!empty($_GET['error'])){
+        if($_GET['error'] == true){
+            // Gambiarra caso de ruim no login.
+            echo "<script>alert('O email ou senha estão errados, por favor, tente novamente.')</script>";
+            echo "<script>location.href='./index.php'</script>";
+        }
+    }
+
+    if(!empty($_GET['user_created'])){
+        if($_GET['user_created'] == true){
+            // Gambiarra caso o usuário seja criado com sucesso.
+            echo "<script>alert('Usuário criado com sucesso! Tente logar na sua conta!')</script>";
+            echo "<script>location.href='./index.php'</script>";
+        }
+    }
+    if(!empty($_GET['logged'])){
+        if($_GET['logged'] == 'false'){
+            // Gambiarra caso o usuário não esteje logado, envia um alerta.
+            echo "<script>alert('Para acessar a home, por favor logue na sua conta.')</script>";
+            echo "<script>location.href='./index.php'</script>";
+        }
+    }
+if(!empty($_GET['password_incorrect'])){
+    if($_GET['password_incorrect'] == 'true'){
+        // Gambiarra caso o usuário não esteje logado, envia um alerta.
+        echo "<script>alert('As senhas utilizadas no cadastro não coincidem.')</script>";
+        echo "<script>location.href='./index.php'</script>";
+    }
+}
+?>
